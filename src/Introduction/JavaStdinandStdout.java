54428a68aba71a5a88854013d40579671305c225
Kchalabi <Khelifa.chalabi@gmail.com>
2021-01-05 18:52:10 +0100

    Java Stdin and Stdout

---
+++ src/Introduction/JavaStdinandStdout.java
@@ -0,0 +1,17 @@
+package Introduction;
+
+import java.util.*;
+
+public class JavaStdinandStdout {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		int c = scn.nextInt();
+		scn.close();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
