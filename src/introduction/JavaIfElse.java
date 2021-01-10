84de68e55bdc1573d4acc55de19a85e0e8c36fe7
Kchalabi <Khelifa.chalabi@gmail.com>
2021-01-10 13:03:28 +0100

    Java if else..

--- src/introduction/JavaIfElse.java
+++ src/introduction/JavaIfElse.java
@@ -1,9 +1,27 @@
 package introduction;
 
+import java.util.*;
+
 public class JavaIfElse {
 
 	public static void main(String[] args) {
+		int n;
+		Scanner scn = new Scanner(System.in);
+		n = scn.nextInt();
+		if (n % 2 != 0) {
+			System.out.println("Weird");
+		}
+		else if ((n % 2 == 0) & (n >= 2) & (n <= 5)){
+			System.out.println("Not Weird");
+		}
+		else if ((n % 2 == 0) & (n >= 6) & (n <= 20)){
+			System.out.println("Weird");
+		}
+		else if ((n % 2 == 0) & (n > 20)){
+			System.out.println("Not Weird");
+		}
 		
+		scn.close();
 	}
 
 }
