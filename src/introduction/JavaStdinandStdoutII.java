42e4a5502c86c887f199f8d85ab5d12775780b53
Kchalabi <Khelifa.chalabi@gmail.com>
2021-01-11 15:39:05 +0100

    Java Stdin and Stdout II

---
+++ src/introduction/JavaStdinandStdoutII.java
@@ -0,0 +1,22 @@
+package introduction;
+
+import java.util.*;
+
+public class JavaStdinandStdoutII {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		int a = scn.nextInt();
+		double b = scn.nextDouble();
+		scn.nextLine();
+		String c = scn.nextLine();
+		
+		System.out.println("String: " + c);
+		System.out.println("Double: " + b);
+		System.out.println("Int: " + a);
+		
+		scn.close();
+	}
+
+}
